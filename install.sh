6ee42d3580b839127cd366d2d1fe50db8192e6ce
Boris Broenner <uykusuz@hintergarten.de>
2017-01-11 09:50:15 +0100

    vim: added NERDTree

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+exitCode=0
+
 echo "Installing base packages ..."
 
 sudo apt-get install vim screen git git-gui
@@ -16,7 +18,6 @@ do
 	# _vimrc -> .vimrc
 	newConfig=`echo $newConfig | sed 's/^_/\./'`
 
-
 	# copy to home directory
 	cp -R $config ~/$newConfig
 done
@@ -24,4 +25,13 @@ done
 echo "Sourcing new bashrc ..."
 source ~/.bashrc
 
+echo "Setting up vim ..."
+if [ -d "~/.vim/bundle/nerdtree/doc" ]; then
+    vim -c "helptags ~/.vim/bundle/nerdtree/doc"
+else
+    (>&2 echo "Error: NERDTree not found! Skipping initialization of it.")
+    exitCode=1
+fi
+
 echo "Done."
+exit $exitCode
