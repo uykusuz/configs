fb24d875c347e256693b02ad7bbc21fb498639ab
Boris Broenner <uykusuz@hintergarten.de>
2018-11-25 21:11:48 +0100

    fix install.sh
    On a fresh linux ran it fully, had to fix some errors.

--- install.sh
+++ install.sh
@@ -11,54 +11,61 @@ if [ ! -f ./_bashrc ]; then
 fi
 
 exitCode=0
+startStep=0
 
-echo "Installing base packages ..."
+if [ $startStep -eq 0 ]; then
+    echo "Installing base packages ..."
 
-sudo pacman -S vim git gitg tmux kdiff3
+    sudo pacman -S vim git tk gitg tmux kdiff3
 
-if [ $? -ne 0 ]; then
-    exit 1
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
 fi
 
-echo "Copying configs ..."
+if [ $startStep -lt 2 ]; then
+    echo "Copying configs ..."
 
-configs=`find . -name "_*"`
+    configs=`find . -name "_*"`
 
-for config in $configs
-do
-	# ./_vimrc -> _vimrc
-	newConfig=`echo $config | sed 's#^\./##'`
+    for config in $configs
+    do
+        # ./_vimrc -> _vimrc
+        newConfig=`echo $config | sed 's#^\./##'`
 
-	# _vimrc -> .vimrc
-	newConfig=`echo $newConfig | sed 's/^_/\./'`
+        # _vimrc -> .vimrc
+        newConfig=`echo $newConfig | sed 's/^_/\./'`
 
-	# copy to home directory
-	cp -R $config ~/$newConfig
-done
+        # copy to home directory
+        cp -R $config ~/$newConfig
+    done
+fi
 
-echo "Copying scripts ..."
-1&>/dev/null mkdir -p ~/bin
-cp ./bin/* ~/bin
+if [ $startStep -lt 3 ]; then
+    echo "Copying scripts ..."
+    1&>/dev/null mkdir -p ~/bin
+    cp ./bin/* ~/bin
+fi
+
+if [ $startStep -lt 4 ]; then
+    echo "Setting up root configs ..."
+    sudo mkdir /root
+    sudo cp _vimrc /root/.vimrc
+fi
+
+if [ $startStep -lt 5 ]; then
+    echo "Setting up vim ..."
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+
+    sudo mkdir -p /root/.vim/bundle
+    sudo git clone https://github.com/VundleVim/Vundle.vim.git /root/.vim/bundle/Vundle.vim
+    sudo vim +PluginInstall +qall
+fi
 
 echo "Sourcing new bashrc ..."
 source ~/.bashrc
 
-echo "Setting up root configs ..."
-sudo cp _vimrc /root/.vimrc
-
-echo "Setting up vim ..."
-homes="~ /root"
-for home in $homes; do
-    mkdir -p $home/.vim/bundle
-    git clone https://github.com/VundleVim/Vundle.vim.git $home/.vim/bundle/Vundle.vim
-done
-
-vim +PluginInstall +qall
-sudo vim +PluginInstall +qall
-
-echo "Installing touchpad configuration ..."
-sudo mkdir -p /etc/X11/xorg.conf.d
-sudo cp 50-synaptics.conf /etc/X11/xorg.conf.d/
-
-echo "Done. Restart X server to activate the touchpad configuration."
+echo "Done."
 exit $exitCode
