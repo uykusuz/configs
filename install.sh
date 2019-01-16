21fd69734842c0974e5305c63ffa7180f3dd07c2
Boris <uykusuz@hintergarten.de>
2019-01-16 21:39:01 +0100

    add i3blocks config

--- install.sh
+++ install.sh
@@ -19,7 +19,9 @@ if [ $startStep -eq 0 ]; then
     sudo pacman -S vim git tk gitg aspell-en tmux fish maven docker
 
     # i3
-    sudo pacman -S i3 dmenu
+    sudo pacman -S i3 dmenu acpi
+    mkdir -p ~/.config
+    git clone https://github.com/vivien/i3blocks-contrib ~/.config/i3blocks
 
     # dependencies for switch-monitor
     sudo pacman -S xorg-xrandr xdotool xorg-xprop xorg-xwininfo wmctrl
@@ -30,6 +32,18 @@ if [ $startStep -eq 0 ]; then
 fi
 
 if [ $startStep -lt 2 ]; then
+    echo "Installing aur packages ..."
+    mkdir -p ~/aur
+
+    cd ~/aur
+
+    git clone https://aur.archlinux.org/ttf-font-awesome-4.git
+    cd ttf-font-awesome-4
+    makepkg -is
+    cd ..
+fi
+
+if [ $startStep -lt 3 ]; then
     echo "Copying configs ..."
 
     configs=`find . -name "_*"`
@@ -47,19 +61,19 @@ if [ $startStep -lt 2 ]; then
     done
 fi
 
-if [ $startStep -lt 3 ]; then
+if [ $startStep -lt 4 ]; then
     echo "Copying scripts ..."
     1&>/dev/null mkdir ~/bin
     cp ./bin/* ~/bin
 fi
 
-if [ $startStep -lt 4 ]; then
+if [ $startStep -lt 5 ]; then
     echo "Setting up root configs ..."
     sudo mkdir /root
     sudo cp _vimrc /root/.vimrc
 fi
 
-if [ $startStep -lt 5 ]; then
+if [ $startStep -lt 6 ]; then
     echo "Setting up vim ..."
     mkdir -p ~/.vim/bundle
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -70,7 +84,7 @@ if [ $startStep -lt 5 ]; then
     sudo vim +PluginInstall +qall
 fi
 
-if [ $startStep -lt 6 ]; then
+if [ $startStep -lt 7 ]; then
     echo "Initializing docker ..."
     sudo systemctl enable docker.service
     sudo systemctl start docker.service
