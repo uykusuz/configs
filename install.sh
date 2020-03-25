83b362431a71c2f1e236cdc5af4bf786a31b4b0f
Boris Brönner <boris.broenner@neuland-bfi.de>
2020-03-25 08:08:32 +0100

    add lazygit as dependency

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ if ! $updateOnly ; then
 
     echo "Installing aur packages ..."
 
-    ${aurInstall} nerd-fonts-complete ttf-font-awesome-4 asdf-vm google-chrome
+    ${aurInstall} nerd-fonts-complete ttf-font-awesome-4 asdf-vm google-chrome lazygit
 
     echo "Setting up zsh ..."
     ${aurInstall} zsh zplug
